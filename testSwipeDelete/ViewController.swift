4db2bbf69b0020c80c4ce284d000b8e041b9501e
RyoNagai <59611366+RyoNagai-8@users.noreply.github.com>
2021-10-28 06:23:15 +0900

    セルにデータを追加

--- testSwipeDelete/ViewController.swift
+++ testSwipeDelete/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet weak var testTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        testTableView.delegate = self
+        testTableView.dataSource = self
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "testCell", for: indexPath)
+        
+        cell.textLabel?.text = "test"
+        
+        return cell
+    }
+    
+    
 }
 
